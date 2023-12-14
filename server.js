1558a6d3ecd60ed5b9f9c3acdf4fb7461adaa628
LucasA75 <lucasacevedocastro@gmail.com>
2023-12-14 10:06:40 -0300

    change method of hash

--- server.js
+++ server.js
@@ -12,13 +12,19 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+/* bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
       console.log(res); //true
     });
-  });
+  }); */
+
+const hashPass  = bcrypt.compareSync(myPlaintextPassword,saltRounds)
+var result = bcrypt.compareSync(myPlaintextPassword, hashPass);
+
+console.log(hashPass)
+console.log(result)
 
 //END_ASYNC
 
